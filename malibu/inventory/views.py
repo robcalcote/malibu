76e6714690facb06b0507395bc3b9fdba67ace08
Unknown <robcalcote@gmail.com>
2018-08-07 20:35:49 -0600

    Created views for inventory list page and details page.

--- malibu/inventory/views.py
+++ malibu/inventory/views.py
@@ -1,3 +1,19 @@
+from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+
+def index(request):
+    products = Product.objects.order_by('descriptionShort_text')
+    context = {'products': products}
+    return render(request, 'inventory/index.html', context)
+
+def detail(request, sku_number):
+    item = get_object_or_404(Product, pk=sku_number)
+    return render(request, 'inventory/detail.html', {'item': item})
+
+
+
+
+# details view page (click the line item link to see more details on the item)
--- malibu/malibu/urls.py
+++ malibu/malibu/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
+    path('inventory/', include('inventory.urls')),
 ]
