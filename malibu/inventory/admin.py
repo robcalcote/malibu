2bb28b0102733f86ec82072d27e0ea9a4e423ba5
John Calcote <john.calcote@gmail.com>
2018-08-24 20:29:42 -0600

    Enhanced admin page for Product and Lot

--- malibu/inventory/admin.py
+++ malibu/inventory/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import Product, Lot
 
 
-admin.site.register(Product)
-admin.site.register(Lot)
+class ProductAdmin(admin.ModelAdmin):
+    fields = ['lot', 'short_description', 'categories', 'weight']
+    list_display = ('short_description', 'lot', 'weight', 'categories')
+    list_filter = ['categories']
+
+
+class LotAdmin(admin.ModelAdmin):
+    fields = ['description', 'purchase_date', 'received']
+    list_display = ('description', 'purchase_date', 'received')
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Lot, LotAdmin)
--- malibu/inventory/models.py
+++ malibu/inventory/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 
-# Create your models here.
 
 class Lot(models.Model):
-    lot_number = models.IntegerField(default=0, primary_key=True)
-    description_text = models.CharField(max_length=128)
-    purchaseDate_date = models.DateField()
-    received_boolean = models.BooleanField()
+    description = models.CharField(max_length=128)
+    purchase_date = models.DateField()
+    received = models.BooleanField()
+
+    def __str__(self):
+        return self.description
+
 
 class Product(models.Model):
-    sku_number = models.IntegerField(default=0, primary_key=True)
-    lot_number = models.ForeignKey('Lot', on_delete=models.PROTECT)
-    categories_text = models.CharField(max_length=256)
-    descriptionShort_text = models.CharField(max_length=80)
-    weight_number = models.DecimalField(max_digits=8, decimal_places=3)
-
-
+    lot = models.ForeignKey('Lot', on_delete=models.PROTECT)
+    categories = models.CharField(max_length=256)
+    short_description = models.CharField(max_length=80)
+    weight = models.DecimalField(max_digits=8, decimal_places=3)
